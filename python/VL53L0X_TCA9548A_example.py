edf46fdfb4d6c1a0db5c4090d777247b1fdf2f0b
Nidulagunawardana <nidulam@outlook.com>
2024-03-12 00:55:40 +0530

    main file changed

--- python/VL53L0X_TCA9548A_example.py
+++ python/VL53L0X_TCA9548A_example.py
@@ -26,9 +26,9 @@ import time
 import VL53L0X
 
 # Create a VL53L0X object for device on TCA9548A bus 1
-tof1 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
+tof1 = VL53L0X.VL53L0X(tca9548a_num=0, tca9548a_addr=0x70)
 # Create a VL53L0X object for device on TCA9548A bus 2
-tof2 = VL53L0X.VL53L0X(tca9548a_num=2, tca9548a_addr=0x70)
+tof2 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
 tof1.open()
 tof2.open()
 
@@ -42,16 +42,17 @@ if timing < 20000:
     timing = 20000
 print("Timing %d ms" % (timing/1000))
 
-for count in range(1, 101):
+while True:
     # Get distance from VL53L0X  on TCA9548A bus 1
-    distance = tof1.get_distance()
-    if distance > 0:
-        print("1: %d mm, %d cm, %d" % (distance, (distance/10), count))
+    distance1 = tof1.get_distance()
+    distance2 = tof2.get_distance()
+    if distance1 > 0:
+        print("1: %d mm, %d mm "% (distance1, (distance2)))
 
     # Get distance from VL53L0X  on TCA9548A bus 2
-    distance = tof2.get_distance()
-    if distance > 0:
-        print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
+
+    # if distance2 > 0:
+    #     print("2: %d mm, %d cm"% (distance2, (distance2/10)))
 
     time.sleep(timing/1000000.00)
 
